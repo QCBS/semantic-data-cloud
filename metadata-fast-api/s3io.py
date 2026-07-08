bc21bfb84205fa1edd2e60f623b79adfa983f0ee
El-Amine Mimouni <el-amine.mimouni@mcgill.ca>
2026-07-08 15:08:00 -0400

    fix indentation level for _materialize_derived_columns() function in s3io.py in metadata-api

--- metadata-fast-api/s3io.py
+++ metadata-fast-api/s3io.py
@@ -135,103 +135,103 @@ def _write_eml_to_duckdb(dataset, content, ddb):
 
 
 def _materialize_derived_columns(ddb):
-        ddb.execute("""
-            WITH extracted_coords AS (
-                SELECT
-                    name,
-                    eml_content,
-                    eml_content -> 'dataset' -> 'coverage' -> 'geographicCoverage' -> 'boundingCoordinates' AS bbox
-                FROM datasets
-            )
+    ddb.execute("""
+        WITH extracted_coords AS (
+            SELECT
+                name,
+                eml_content,
+                eml_content -> 'dataset' -> 'coverage' -> 'geographicCoverage' -> 'boundingCoordinates' AS bbox
+            FROM datasets
+        )
 
-            UPDATE datasets
-            SET
-                min_lon = CAST(bbox ->> 'westBoundingCoordinate' AS DOUBLE),
-                min_lat = CAST(bbox ->> 'southBoundingCoordinate' AS DOUBLE),
-                max_lon = CAST(bbox ->> 'eastBoundingCoordinate' AS DOUBLE),
-                max_lat = CAST(bbox ->> 'northBoundingCoordinate' AS DOUBLE)
-            FROM extracted_coords
-            WHERE datasets.name = extracted_coords.name;
-        """)
+        UPDATE datasets
+        SET
+            min_lon = CAST(bbox ->> 'westBoundingCoordinate' AS DOUBLE),
+            min_lat = CAST(bbox ->> 'southBoundingCoordinate' AS DOUBLE),
+            max_lon = CAST(bbox ->> 'eastBoundingCoordinate' AS DOUBLE),
+            max_lat = CAST(bbox ->> 'northBoundingCoordinate' AS DOUBLE)
+        FROM extracted_coords
+        WHERE datasets.name = extracted_coords.name;
+    """)
 
-        ddb.execute("""
-            WITH exploded_temporal AS (
-                SELECT
-                    datasets.name,
-                    CAST(
-                        COALESCE(
-                            je.value -> 'rangeOfDates' -> 'beginDate' ->> 'calendarDate',
-                            je.value -> 'singleDateTime' ->> 'calendarDate'
-                        ) AS DATE
-                    ) AS start_date,
-                    CAST(
-                        COALESCE(
-                            je.value -> 'rangeOfDates' -> 'endDate' ->> 'calendarDate',
-                            je.value -> 'singleDateTime' ->> 'calendarDate'
-                        ) AS DATE
-                    ) AS end_date
-                FROM datasets,
-                json_each(
-                    CASE
-                        WHEN json_type(datasets.eml_content -> 'dataset' -> 'coverage' -> 'temporalCoverage') = 'ARRAY'
-                            THEN datasets.eml_content -> 'dataset' -> 'coverage' -> 'temporalCoverage'
-                        ELSE json_array(datasets.eml_content -> 'dataset' -> 'coverage' -> 'temporalCoverage')
-                    END
-                ) AS je
-            ),
+    ddb.execute("""
+        WITH exploded_temporal AS (
+            SELECT
+                datasets.name,
+                CAST(
+                    COALESCE(
+                        je.value -> 'rangeOfDates' -> 'beginDate' ->> 'calendarDate',
+                        je.value -> 'singleDateTime' ->> 'calendarDate'
+                    ) AS DATE
+                ) AS start_date,
+                CAST(
+                    COALESCE(
+                        je.value -> 'rangeOfDates' -> 'endDate' ->> 'calendarDate',
+                        je.value -> 'singleDateTime' ->> 'calendarDate'
+                    ) AS DATE
+                ) AS end_date
+            FROM datasets,
+            json_each(
+                CASE
+                    WHEN json_type(datasets.eml_content -> 'dataset' -> 'coverage' -> 'temporalCoverage') = 'ARRAY'
+                        THEN datasets.eml_content -> 'dataset' -> 'coverage' -> 'temporalCoverage'
+                    ELSE json_array(datasets.eml_content -> 'dataset' -> 'coverage' -> 'temporalCoverage')
+                END
+            ) AS je
+        ),
 
-            aggregated AS (
-                SELECT
-                    name,
-                    MIN(start_date) AS begin_date,
-                    MAX(end_date) AS end_date
-                FROM exploded_temporal
-                GROUP BY name
-            )
+        aggregated AS (
+            SELECT
+                name,
+                MIN(start_date) AS begin_date,
+                MAX(end_date) AS end_date
+            FROM exploded_temporal
+            GROUP BY name
+        )
 
-            UPDATE datasets
-            SET
-                begin_date = aggregated.begin_date,
-                end_date = aggregated.end_date
-            FROM aggregated
-            WHERE datasets.name = aggregated.name;
-        """)
+        UPDATE datasets
+        SET
+            begin_date = aggregated.begin_date,
+            end_date = aggregated.end_date
+        FROM aggregated
+        WHERE datasets.name = aggregated.name;
+    """)
 
-        ddb.execute("""
-            WITH extracted_license AS (
-                SELECT
-                    name,
-                    eml_content,
-                    eml_content -> 'dataset' -> 'licensed' ->> 'identifier' AS license_id
-                FROM datasets
-            )
+    ddb.execute("""
+        WITH extracted_license AS (
+            SELECT
+                name,
+                eml_content,
+                eml_content -> 'dataset' -> 'licensed' ->> 'identifier' AS license_id
+            FROM datasets
+        )
 
-            UPDATE datasets
-            SET
-                license_id = extracted_license.license_id
-            FROM extracted_license
-            WHERE datasets.name = extracted_license.name;
-        """)
+        UPDATE datasets
+        SET
+            license_id = extracted_license.license_id
+        FROM extracted_license
+        WHERE datasets.name = extracted_license.name;
+    """)
 
-        ddb.execute("""
-            WITH extracted_citation AS (
-                SELECT
-                    name,
-                    eml_content,
-                    CASE
-                        WHEN json_type(eml_content -> 'additionalMetadata' -> 'metadata' -> 'gbif' -> 'citation') = 'OBJECT'
-                            THEN eml_content -> 'additionalMetadata' -> 'metadata' -> 'gbif' -> 'citation' ->> 'citation'
-                        ELSE eml_content -> 'additionalMetadata' -> 'metadata' -> 'gbif' ->> 'citation'
-                    END AS dataset_citation
-                FROM datasets
-            )
+    ddb.execute("""
+        WITH extracted_citation AS (
+            SELECT
+                name,
+                eml_content,
+                CASE
+                    WHEN json_type(eml_content -> 'additionalMetadata' -> 'metadata' -> 'gbif' -> 'citation') = 'OBJECT'
+                        THEN eml_content -> 'additionalMetadata' -> 'metadata' -> 'gbif' -> 'citation' ->> 'citation'
+                    ELSE eml_content -> 'additionalMetadata' -> 'metadata' -> 'gbif' ->> 'citation'
+                END AS dataset_citation
+            FROM datasets
+        )
 
-            UPDATE datasets
-            SET
-                dataset_citation = extracted_citation.dataset_citation
-            FROM extracted_citation
-            WHERE datasets.name = extracted_citation.name;
-        """)
+        UPDATE datasets
+        SET
+            dataset_citation = extracted_citation.dataset_citation
+        FROM extracted_citation
+        WHERE datasets.name = extracted_citation.name;
+    """)
 
 
 def s3_to_duckdb(target_name, extension, ddb, max_workers=20):
